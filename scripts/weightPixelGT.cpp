d3e8a15c8d7ff9e1ac84896af649293603f1940d
Brian Davis <herobd@gmail.com>
2017-04-25 17:16:03 -0600

    Pixel GT weighting working (runs only single image)

--- scripts/weightPixelGT.cpp
+++ scripts/weightPixelGT.cpp
@@ -1,131 +1,36 @@
-//g++ -std=c++11 -fopenmp weightPixelGT.cpp -lcaffe -lglog -l:libopencv_core.so.3.1 -l:libopencv_imgcodecs.so.3.1 -l:libopencv_imgproc.so.3.1 -lprotobuf -lboost_system -I ../include/ -L ../build/lib/ -o weightPixelGT
+//g++ -g -std=c++11 weightPixelGT.cpp -lopencv_core -lopencv_imgcodecs -lopencv_imgproc -o weightPixelGT
 
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <opencv2/imgcodecs.hpp>
 #include <vector>
+#include <map>
 #include <tuple>
+#include <limits>
+#include <iostream>
+
+#define INT_POS_INFINITY (numeric_limits<int>::max())
 
 using namespace std;
 using namespace cv;
 
-//////////ZHANG SKELTONIZATION///////////////////////////////////////
-///////////from https://github.com/bsdnoobz/zhang-suen-thinning//////
-void thinningIteration(cv::Mat& img, int iter)
-{
-    CV_Assert(img.channels() == 1);
-    CV_Assert(img.depth() != sizeof(uchar));
-    CV_Assert(img.rows > 3 && img.cols > 3);
-
-    cv::Mat marker = cv::Mat::zeros(img.size(), CV_8UC1);
-
-    int nRows = img.rows;
-    int nCols = img.cols;
-
-    if (img.isContinuous()) {
-        nCols *= nRows;
-        nRows = 1;
-    }
-
-    int x, y;
-    uchar *pAbove;
-    uchar *pCurr;
-    uchar *pBelow;
-    uchar *nw, *no, *ne;    // north (pAbove)
-    uchar *we, *me, *ea;
-    uchar *sw, *so, *se;    // south (pBelow)
-
-    uchar *pDst;
-
-    // initialize row pointers
-    pAbove = NULL;
-    pCurr  = img.ptr<uchar>(0);
-    pBelow = img.ptr<uchar>(1);
-
-    for (y = 1; y < img.rows-1; ++y) {
-        // shift the rows up by one
-        pAbove = pCurr;
-        pCurr  = pBelow;
-        pBelow = img.ptr<uchar>(y+1);
-
-        pDst = marker.ptr<uchar>(y);
-
-        // initialize col pointers
-        no = &(pAbove[0]);
-        ne = &(pAbove[1]);
-        me = &(pCurr[0]);
-        ea = &(pCurr[1]);
-        so = &(pBelow[0]);
-        se = &(pBelow[1]);
-
-        for (x = 1; x < img.cols-1; ++x) {
-            // shift col pointers left by one (scan left to right)
-            nw = no;
-            no = ne;
-            ne = &(pAbove[x+1]);
-            we = me;
-            me = ea;
-            ea = &(pCurr[x+1]);
-            sw = so;
-            so = se;
-            se = &(pBelow[x+1]);
-
-            int A  = (*no == 0 && *ne >= 1) + (*ne == 0 && *ea >= 1) +
-                     (*ea == 0 && *se >= 1) + (*se == 0 && *so >= 1) +
-                     (*so == 0 && *sw >= 1) + (*sw == 0 && *we >= 1) +
-                     (*we == 0 && *nw >= 1) + (*nw == 0 && *no >= 1);
-            int B  = *no + *ne + *ea + *se + *so + *sw + *we + *nw;
-            int m1 = iter == 0 ? (*no * *ea * *so) : (*no * *ea * *we);
-            int m2 = iter == 0 ? (*ea * *so * *we) : (*no * *so * *we);
-
-            if (A == 1 && (B >= 2 && B <= 6) && m1 == 0 && m2 == 0)
-                pDst[x] = 1;
-        }
-    }
-
-    img &= ~marker;
-}
-
-/**
- * Function for thinning the given binary image
- *
- * Parameters:
- *              src  The source image, binary with range = [0,255], 0 being off
- *              dst  The destination image
- */
-void thinning(const cv::Mat& src, cv::Mat& dst)
-{
-    dst = src.clone();
-
-    cv::Mat prev = cv::Mat::zeros(dst.size(), CV_8UC1);
-    cv::Mat diff;
-
-    do {
-        thinningIteration(dst, 0);
-        thinningIteration(dst, 1);
-        cv::absdiff(dst, prev, diff);
-        dst.copyTo(prev);
-    }
-    while (cv::countNonZero(diff) > 0);
-
-    dst *= 255;
-
-    cleanNoiseFromEdges(dst);
-}
 /////////////////////////////////////////////////////////
 //Meijster distance <http://fab.cba.mit.edu/classes/S62.12/docs/Meijster_distance.pdf>
 //This can be parallelized. Should probably flip from column to row first
 int f(int x, int i, int y, const Mat& g);
 int SepPlusOne(int i, int u, int y, const Mat& g);
-void computeDistanceMapForCC(const Mat& ccMap, int ccId, int minX, int minY, int maxX, int maxY, 
-                            Mat& out, Mat& g, int* s, int* t)
+void computeDistanceMap(const Mat& img, const Mat& ccMap, int capDist Mat& out)
 {
-    //Mat out(src.size(), src.type());
+    map<int,int> ccMaxDists;
+    ccMaxDists[0]=1;
+    Mat g(img.size(), CV_32S);
+    int s[img.cols];         
+    int t[img.cols];        
     int maxDist=0;
     //Mat g(ccMap.size(), ccMap.type());
-    for (int x=minX; x<=maxX; x++)
+    for (int x=0; x<=img.cols-1; x++)
     {
-        if (ccMap.at<unsigned short>(0,x)!=ccId)
+        if (img.at<unsigned char>(0,x)==0)
         {
             g.at<int>(0, x)=0;
         }
@@ -134,9 +39,9 @@ void computeDistanceMapForCC(const Mat& ccMap, int ccId, int minX, int minY, int
             g.at<int>(0, x)=INT_POS_INFINITY;//src.cols*src.rows;
         }
 
-        for (int y=minY+1; y<=maxY; y++)
+        for (int y=0+1; y<=img.rows-1; y++)
         {
-            if (ccMap.at<unsigned short>(y,x)!=ccId)
+            if (img.at<unsigned char>(y,x)==0)
             {
                 g.at<int>(y, x)=0;
             }
@@ -149,7 +54,7 @@ void computeDistanceMapForCC(const Mat& ccMap, int ccId, int minX, int minY, int
             }
         }
 
-        for (int y=maxY-1; y>=minY; y--)
+        for (int y=img.rows-1-1; y>=0; y--)
         {
             if (g.at<int>((y+1), x)<g.at<int>(y, x))
             {
@@ -159,41 +64,33 @@ void computeDistanceMapForCC(const Mat& ccMap, int ccId, int minX, int minY, int
                     g.at<int>(y, x) = INT_POS_INFINITY;
             }
         }
-
-        /*if(x==20)
-        {
-            for (int y=0; y<src.rows; y++)
-            {
-                printf("%d .. %d\n",qGray(src.pixel(x,y)),g.at<int>(y, x));
-            }
-        }*/
     }
 
     int q;
-    //int s[ccMap.cols];
-    //int t[ccMap.cols];
+   
+ 
     int w;
-    for (int y=minY; y<=maxY; y++)
+    for (int y=0; y<=img.rows-1; y++)
     {
-        q=minX;
-        s[minX]=minX;
-        t[minX]=minX;
-        for (int u=minX+1; u<=maxX; u++)
+        q=0;
+        s[0]=0;
+        t[0]=0;
+        for (int u=0+1; u<=img.cols-1; u++)
         {
-            while (q>=minX && f(t[q],s[q],y,g) > f(t[q],u,y,g))
+            while (q>=0 && f(t[q],s[q],y,g) > f(t[q],u,y,g))
             {
                 q--;
             }
 
-            if (q<minX)
+            if (q<0)
             {
-                q=minX;
-                s[minX]=u;
+                q=0;
+                s[0]=u;
             }
             else
             {
                 w = SepPlusOne(s[q],u,y,g);
-                if (w<=maxX)
+                if (w<=img.cols-1)
                 {
                     q++;
                     s[q]=u;
@@ -202,33 +99,37 @@ void computeDistanceMapForCC(const Mat& ccMap, int ccId, int minX, int minY, int
             }
         }
 
-        for (int u=maxX; u>=minX; u--)
+        for (int u=img.cols-1; u>=0; u--)
         {
-            out.at<unsigned char>(y, u)= min(f(u,s[q],y,g),100);
-            if (out.at<unsigned char>(y, u) > maxDist)
-                maxDist = out.at<unsigned char>(y, u);
+            unsigned char d = std::min(std::sqrt(f(u,s[q],y,g)),0.0+capDist);
+            if (ccMap.at<unsigned short>(y,u)!=0)
+            {
+                out.at<unsigned char>(y, u)= d;
+                if (d > ccMaxDists[ccMap.at<unsigned short>(y,u)])
+                    ccMaxDists[ccMap.at<unsigned short>(y,u)] = d;
+            }
             if (u==t[q])
                 q--;
         }
     }
+    //cout<<"max d: "<<maxDist<<endl;
 
-    //Normalization for CC
-    for (int x=minX; x<=maxX; x++)
-        for (int y=minY; y<=maxY; y++)
-            out.at<unsigned char>(y, x) = 255 * out.at<unsigned char>(y, x)/(maxDist+0.0);
+    for (int x=0; x<=img.cols-1; x++)
+        for (int y=0; y<=img.rows-1; y++)
+            out.at<unsigned char>(y, x) = 255 * out.at<unsigned char>(y, x)/(ccMaxDists[ccMap.at<unsigned short>(y,x)]+0.0);
 }
 
 int SepPlusOne(int i, int u, int y, const Mat& g)
 {
-    if (g.at<unsigned char>(y,u) == INT_POS_INFINITY)
+    if (g.at<int>(y,u) == INT_POS_INFINITY)
         return INT_POS_INFINITY;
-    return 1 + ((u*u)-(i*i)+g.at<unsigned char>(y,u)*g.at<unsigned char>(y,u)-(g.at<unsigned char>(y,i)*g.at<unsigned char>(y,i))) / (2*(u-i));
+    return 1 + ((u*u)-(i*i)+g.at<int>(y,u)*g.at<int>(y,u)-(g.at<int>(y,i)*g.at<int>(y,i))) / (2*(u-i));
 }
 int f(int x, int i, int y, const Mat& g)
 {
-    if (g.at<unsigned char>(y,i)==INT_POS_INFINITY || x==INT_POS_INFINITY)
+    if (g.at<int>(y,i)==INT_POS_INFINITY || x==INT_POS_INFINITY)
         return INT_POS_INFINITY;
-    return (x-i)*(x-i) + g.at<unsigned char>(y,i)*g.at<unsigned char>(y,i);
+    return (x-i)*(x-i) + g.at<int>(y,i)*g.at<int>(y,i);
 }
 
 Mat getCCs(const Mat& src, vector<tuple<int,int,int,int> >* ccBounds)
@@ -242,14 +143,14 @@ Mat getCCs(const Mat& src, vector<tuple<int,int,int,int> >* ccBounds)
             int cc = ccMap.at<unsigned short>(r,c);
             if (cc>0)
             {
-                if (c < get<0>(ccBounds.at(cc-1)))
-                    get<0>(ccBounds.at(cc-1)) = c;
-                if (r < get<1>(ccBounds.at(cc-1)))
-                    get<1>(ccBounds.at(cc-1)) = r;
-                if (c > get<2>(ccBounds.at(cc-1)))
-                    get<2>(ccBounds.at(cc-1)) = c;
-                if (r > get<3>(ccBounds.at(cc-1)))
-                    get<3>(ccBounds.at(cc-1)) = r;
+                if (c < get<0>(ccBounds->at(cc-1)))
+                    get<0>(ccBounds->at(cc-1)) = c;
+                if (r < get<1>(ccBounds->at(cc-1)))
+                    get<1>(ccBounds->at(cc-1)) = r;
+                if (c > get<2>(ccBounds->at(cc-1)))
+                    get<2>(ccBounds->at(cc-1)) = c;
+                if (r > get<3>(ccBounds->at(cc-1)))
+                    get<3>(ccBounds->at(cc-1)) = r;
             }
         }
     return ccMap;
@@ -265,35 +166,47 @@ Mat getCCs(const Mat& src, vector<tuple<int,int,int,int> >* ccBounds)
    */
 
 //this assumes a binary image. >0 on, 0 off
-Mat weight(const Mat& img, int dilate_size)
+Mat weight(const Mat& img, int dilate_size, int capDist)
 {
     Mat element = getStructuringElement( MORPH_ELLIPSE,
                                          Size( 2*dilate_size + 1, 2*dilate_size+1 ),
                                          Point( dilate_size, dilate_size ) );
+    Mat elementBig = getStructuringElement( MORPH_ELLIPSE,
+                                         Size( 4*dilate_size + 1, 4*dilate_size+1 ),
+                                         Point( 2*dilate_size, 2*dilate_size ) );
     Mat dilated;
-    dilate( img, dilated, element );
+    if (dilate_size>0)
+    {
+        /*
+        //Close
+        dilate( img, dilated, elementBig );
+        erode( dilated, dilated, elementBig );
+        //Dilate
+        dilate( dilated, dilated, element );
+        */
+        dilate( img, dilated, element );
+    }
+    else
+        dilated=img;
 
     vector<tuple<int,int,int,int> > ccBounds;//minX,minY,maxX,maxY
-    Mat ccMap = getCCS(dilated,&ccBounds);
+    Mat ccMap = getCCs(dilated,&ccBounds);
     
 
-    Mat distanceMap(img.size(), img.type());
-    Mat workG(img.size(), CV_32S); //for memory effiency
-    int workS[img.cols];               // "
-    int workT[img.cols];               // "
-    for (int cc=1; cc<=numCCs; cc++)
-    {
-        computeDistanceMapForCC(ccMap,cc,get<0>(ccBounds[cc-1]),get<1>(ccBounds[cc-1]),get<2>(ccBounds[cc-1]),get<3>(ccBounds[cc-1]), distanceMap,workG,workS,workT);
-    }
+    Mat distanceMap = Mat::zeros(img.size(), img.type());
+    computeDistanceMap(dilated, ccMap, capDist, distanceMap);
     return distanceMap;
 }
 
 int main(int argc, char** argv)
 {
-    string inFile = argv[0];
-    string outFile = argv[1];
-    Mat in = imread(inFile,CV_LOAD_IMAGE_GRAYSCALE);
-    Mat out = weight(in,5);
+    int dilate=5;
+    int capDist=10;
+    string inFile = argv[1];
+    string outFile = argv[2];
+    Mat in = imread(inFile,0);//CV_LOAD_IMAGE_GRAYSCALE);
+    assert(in.channels()==1);
+    Mat out = weight(in,dilate,capDist);
     imwrite(outFile,out);
     return 0;
 }
