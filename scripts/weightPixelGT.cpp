bde8ed00f89edcf66d6645950585e289a5266a02
Brian Davis <herobd@gmail.com>
2017-04-28 17:59:06 -0600

    Weighting code now does psuedo prec weight. Still bugs on weird edges

--- scripts/weightPixelGT.cpp
+++ scripts/weightPixelGT.cpp
@@ -4,12 +4,14 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include <opencv2/imgcodecs.hpp>
 #include <vector>
+#include <set>
 #include <map>
 #include <tuple>
 #include <limits>
 #include <iostream>
 
 #define INT_POS_INFINITY (numeric_limits<int>::max())
+#define FLOAT_POS_INFINITY (numeric_limits<float>::max())
 
 using namespace std;
 using namespace cv;
@@ -19,7 +21,7 @@ using namespace cv;
 //This can be parallelized. Should probably flip from column to row first
 int f(int x, int i, int y, const Mat& g);
 int SepPlusOne(int i, int u, int y, const Mat& g);
-void computeDistanceMap(const Mat& img, const Mat& ccMap, int capDist Mat& out)
+void computeDistanceMap(const Mat& orig, const Mat& img, const Mat& ccMap, int capDist, Mat& out)
 {
     map<int,int> ccMaxDists;
     ccMaxDists[0]=1;
@@ -105,6 +107,7 @@ void computeDistanceMap(const Mat& img, const Mat& ccMap, int capDist Mat& out)
             if (ccMap.at<unsigned short>(y,u)!=0)
             {
                 out.at<unsigned char>(y, u)= d;
+                //store max distance by CC
                 if (d > ccMaxDists[ccMap.at<unsigned short>(y,u)])
                     ccMaxDists[ccMap.at<unsigned short>(y,u)] = d;
             }
@@ -114,9 +117,13 @@ void computeDistanceMap(const Mat& img, const Mat& ccMap, int capDist Mat& out)
     }
     //cout<<"max d: "<<maxDist<<endl;
 
+    //Normalize CCs
     for (int x=0; x<=img.cols-1; x++)
         for (int y=0; y<=img.rows-1; y++)
-            out.at<unsigned char>(y, x) = 255 * out.at<unsigned char>(y, x)/(ccMaxDists[ccMap.at<unsigned short>(y,x)]+0.0);
+            if (orig.at<unsigned char>(y,x)>0) //clip to original "on" pixels
+                out.at<unsigned char>(y, x) = 255 * out.at<unsigned char>(y, x)/(ccMaxDists[ccMap.at<unsigned short>(y,x)]+0.0);
+            else
+                out.at<unsigned char>(y, x)=0;
 }
 
 int SepPlusOne(int i, int u, int y, const Mat& g)
@@ -132,6 +139,7 @@ int f(int x, int i, int y, const Mat& g)
     return (x-i)*(x-i) + g.at<int>(y,i)*g.at<int>(y,i);
 }
 
+/*
 Mat getCCs(const Mat& src, vector<tuple<int,int,int,int> >* ccBounds)
 {
     Mat ccMap(src.size(), CV_16U);
@@ -155,18 +163,18 @@ Mat getCCs(const Mat& src, vector<tuple<int,int,int,int> >* ccBounds)
         }
     return ccMap;
 }
+*/
 
 
 /*
    Get CC
-   dialate the inverted image (make gt area larger)
-   invert image
-   Run the distance transform on inverted image (gets distance from background, aka the orders) (by CC?)
-   Normalize weights by CC
+   dialate the image (make gt area larger)
+   Run the distance transform on ("inverted") image (gets distance from background, aka the orders)
+   Normalize CCs individually
    */
 
 //this assumes a binary image. >0 on, 0 off
-Mat weight(const Mat& img, int dilate_size, int capDist)
+Mat recallWeight(const Mat& img, int dilate_size, int capDist)
 {
     Mat element = getStructuringElement( MORPH_ELLIPSE,
                                          Size( 2*dilate_size + 1, 2*dilate_size+1 ),
@@ -189,24 +197,160 @@ Mat weight(const Mat& img, int dilate_size, int capDist)
     else
         dilated=img;
 
-    vector<tuple<int,int,int,int> > ccBounds;//minX,minY,maxX,maxY
-    Mat ccMap = getCCs(dilated,&ccBounds);
+    //vector<tuple<int,int,int,int> > ccBounds;//minX,minY,maxX,maxY
+    //Mat ccMap = getCCs(dilated,&ccBounds);
+    Mat ccMap(dilated.size(), CV_16U);
+    int count = connectedComponents (dilated, ccMap, 8, CV_16U);
     
 
     Mat distanceMap = Mat::zeros(img.size(), img.type());
-    computeDistanceMap(dilated, ccMap, capDist, distanceMap);
+    computeDistanceMap(img,dilated, ccMap, capDist, distanceMap);
     return distanceMap;
+}
+
+
+
+void writeWeights(Mat& out, int startR, int startC, int norm, const Mat& values, const Mat& trace)
+{
+    int r=startR;
+    int c=startC;
+    while(values.at<float>(r,c)>0)
+    {
+        out.at<unsigned char>(r,c) = 128 + (127*values.at<float>(r,c))/norm;
+        int t = trace.at<int>(r,c);
+        r = t%trace.rows;
+        c = t/trace.rows;
+        assert(sqrt(pow(r-startR,2)+pow(c-startC,2)) <= norm+1);
+        assert(values.at<float>(r,c)<values.at<float>(startR,startC));
+    }
+}
+void writeWeights(Mat& out, int r, int c, int norm, const Mat& values, const vector< set<int> >& trace)
+{
+    if(values.at<float>(r,c)>0)
+    {
+        out.at<unsigned char>(r,c) = min(255,(int)(128 + (127*values.at<float>(r,c))/norm));//min((unsigned char)(128 + (127*values.at<float>(r,c))/norm),out.at<unsigned char>(r,c));
+        for (int t : trace.at(r+c*values.rows))
+        {
+            int newR = t%values.rows;
+            int newC = t/values.rows;
+            writeWeights(out,newR,newC,norm,values,trace);
+            //assert(out.at<unsigned char>(0,0)==0);
+        }
+    }
+}
+
+Mat precWeight(const Mat& img, int maxSize)
+{
+    Mat ccMap;
+    int count = connectedComponents (img, ccMap, 8, CV_16U);
+    Mat waveMap = img.clone();//Mat::zeros(img.size(),CV_16UC3); //waveNum,trace,cc
+    waveMap = 1-(waveMap/255);
+    waveMap.convertTo(waveMap,CV_32F);
+    waveMap*=FLOAT_POS_INFINITY;
+    //vector<Mat> channels(3);
+    //channels.at(0)=waveMap;
+    //channels.at(1)=Mat(img.size(),CV_32U);//trace x+y*cols
+    //channels.at(2)=ccMap;
+    //merge(channels,waveMap);
+    //Mat trace = Mat(img.size(),CV_32S);//trace x+y*cols
+    
+    //map<int,set<int> > trace;
+    vector< set<int> > trace(img.cols*img.rows);
+
+    Mat ret(img.size(), CV_8U);
+    ret=128;
+
+    int minR=0;
+    int maxR=img.rows;
+    int minC=0;
+    int maxC=img.cols;
+    int nMinR=minR;
+    int nMaxR=maxR;
+    int nMinC=minC;
+    int nMaxC=maxC;
+    for (int wave=1; wave<=maxSize; wave++)
+    {
+        set<tuple<int,int,float> > colliding;
+        for (int r=minR; r<maxR; r++)
+            for (int c=minC; c<maxC; c++)
+            {
+                if (waveMap.at<float>(r,c)<wave && waveMap.at<float>(r,c)>=wave-1)
+                {
+                    //Any INF neighbors?
+                    //Any different CC neighbors?
+                    for (int rd=-1; rd<=1; rd++)
+                        for (int cd=-1; cd<=1; cd++)
+                        {
+                            if ((rd==0 && cd==0) || r+rd<0 || r+rd>=waveMap.rows || c+cd<0 || c+cd>=waveMap.cols)
+                                continue;
+                            float distance = waveMap.at<float>(r,c) + (rd==0||cd==0?1:1.41421356237);
+                            bool collide=ccMap.at<unsigned short>(r+rd,c+cd)!=0 && ccMap.at<unsigned short>(r+rd,c+cd)!=ccMap.at<unsigned short>(r,c);
+                            if (waveMap.at<float>(r+rd,c+cd)>=distance)
+                            {//move wave front here
+                                waveMap.at<float>(r+rd,c+cd)=distance;
+                                ccMap.at<unsigned short>(r+rd,c+cd)=ccMap.at<unsigned short>(r,c);
+                                //trace[r+rd + (c+cd)*img.rows].insert(r + c*img.rows);
+                                trace.at(r+rd + (c+cd)*img.rows).insert(r + c*img.rows);
+                            }
+                            if (collide)
+                            {//wave front collision
+                                float norm = max(waveMap.at<float>(r+rd,c+cd),waveMap.at<float>(r,c));
+                                colliding.emplace(r,c,norm);
+                                colliding.emplace(r+rd,c+cd,norm);
+                                //writeWeights(ret,r,c,norm,waveMap,trace);
+                                //writeWeights(ret,r+rd,c+cd,norm,waveMap,trace);
+                            }
+                        }
+                    if (wave==1)
+                    {
+                        if (r>nMaxR)
+                            nMaxR=r;
+                        if (r<nMinR)
+                            nMinR=r;
+                        if (c>nMaxC)
+                            nMaxC=c;
+                        if (c<nMinC)
+                            nMinC=c;
+                    }
+            //assert(ret.at<unsigned char>(0,0)==0);
+                }
+            }
+        for (auto t : colliding)
+        {
+            writeWeights(ret,get<0>(t),get<1>(t),get<2>(t),waveMap,trace);
+        }
+        if (wave==1)
+        {
+            maxR=nMaxR+2;
+            minR=nMinR-1;
+            maxC=nMaxC+2;
+            minC=nMinC-1;
+        }
+        //expand our RoI to allow expansion of wavefront
+        maxR = min(img.rows,maxR+1);
+        maxC = min(img.cols,maxC+1);
+        minR = max(0,minR-1);
+        minC = max(0,minC-1);
+
+        cout<<"wavefront "<<wave<<" complete."<<endl;
+    }
+
+    return ret;
 }
 
 int main(int argc, char** argv)
 {
-    int dilate=5;
-    int capDist=10;
+    int dilate=3;
+    int capDist=15;
+    int maxDist=10;
     string inFile = argv[1];
-    string outFile = argv[2];
+    string outRecallFile = argv[2];
+    string outPrecFile = argv[3];
     Mat in = imread(inFile,0);//CV_LOAD_IMAGE_GRAYSCALE);
     assert(in.channels()==1);
-    Mat out = weight(in,dilate,capDist);
-    imwrite(outFile,out);
+    Mat outPrec = precWeight(in,maxDist);
+    imwrite(outPrecFile,outPrec);
+    Mat outRecall = recallWeight(in,dilate,capDist);
+    imwrite(outRecallFile,outRecall);
     return 0;
 }
